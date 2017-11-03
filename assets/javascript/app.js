05abd2a256b819bcf3e58814d1ff012df84d9879
ronronrecords <info@ronronrecords.com>
2017-11-03 12:07:01 -0700

    new changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,32 @@
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
 
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.text(minutes + ":" + seconds);
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+jQuery(function ($) {
+    var fiveMinutes = 58 * 1,
+        display = $('#time');
+    startTimer(fiveMinutes, display);
+    if (fiveMinutes === 0) {
+        alert("TIME UP!");
+    }
+});
+
+function pageReload() {
+    location.reload();
+}
+$("#t").click(pageReload);
+
+$("#f").click(pageReload);
